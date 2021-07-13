a5669ecb363289528a6fdffc8a16c0f9a3731dfd
Alexplus77 <alexplus77@mail.ru>
2021-07-13 15:01:08 +0200

    HHH

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,7 +4,7 @@ let count = document.querySelector("#timer").textContent;
 function timer() {
   if (count > 0) {
     count--;
-    document.querySelector("#timer").textContent = count;
+    document.querySelector("#timer").textContent =`00:00:${count}`;
     return;
   }
   alert("Вы победили в конкурсе!");
