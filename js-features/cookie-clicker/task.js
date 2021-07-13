79ebf46732e129383ce0ca5b1bcc14e700a83a50
Alexplus77 <alexplus77@mail.ru>
2021-07-13 17:04:01 +0200

    lll

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -7,13 +7,13 @@ let countTime = new Date().getSeconds() - countSeconds
 
 function click() {
     document.querySelector("#clicker__counter").textContent = count++;  
-   
+   countSeconds = new Date().getSeconds()
     if (count % 2) {
         imgClick.width = 400
     } else {
         imgClick.width = 200
     }
-    countSeconds = new Date().getSeconds()
+    
     
 console.log(countTime)
 }
