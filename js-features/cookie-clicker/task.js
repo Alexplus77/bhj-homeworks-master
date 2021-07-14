a0b098bb844908058883bafe08aacdb01f349c87
Alexplus77 <alexplus77@mail.ru>
2021-07-14 13:46:35 +0200

    ddd

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,22 +1,9 @@
+//! Игра-кликер.
 let count = document.querySelector("#clicker__counter").textContent;
 let imgClick = document.querySelector("img");
-imgClick.addEventListener('click', click)
-
-let countTime = 0
-
+imgClick.addEventListener("click", click);
 
 function click() {
-    
-   
-    document.querySelector("#clicker__counter").textContent = count++;
-    //count % 2 ? (imgClick.width = 400) : (imgClick.width = 200);
-  
-    if (count % 2) {
-        imgClick.width = 400
-    } else {
-        imgClick.width = 200
-    }
-    console.log(countTime)    
-    }
-
-
+  document.querySelector("#clicker__counter").textContent = count++;
+  count % 2 ? (imgClick.width = 400) : (imgClick.width = 200);
+}
