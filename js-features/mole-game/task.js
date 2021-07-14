cdd563866251c6b41c408d196c128683b31c1626
Alexplus77 <alexplus77@mail.ru>
2021-07-14 21:41:43 +0200

    fff

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,11 +1,13 @@
 const holeAll = document.querySelector(".hole-game");
 let lost = 0;
 let dead = 0;
+const statusWin = 10;
+const statusLost = 5;
 const findHole = (event) => {
   if (event.target.classList.contains("hole_has-mole")) {
-    document.querySelector("#dead").textContent = dead++;
+    document.querySelector("#dead").textContent = ++dead;
   } else {
-    document.querySelector("#lost").textContent = lost++;
+    document.querySelector("#lost").textContent = ++lost;
   }
   win();  
 };
@@ -13,17 +15,19 @@ const findHole = (event) => {
 holeAll.addEventListener("click", findHole);
 
 function win() {
-  if (dead === 10 && lost <= 5) {
-    alert("Вы выиграли");
-    dead = 0;
-    lost = 0;
-    document.querySelector("#dead").textContent = dead;
-    document.querySelector("#lost").textContent = lost;
-  } else if (lost > 5) {
-    alert("Вы проиграли");
-    dead = 0;
-    lost = 0;
-    document.querySelector("#dead").textContent = dead;
-    document.querySelector("#lost").textContent = lost;
+  if (dead === statusWin && lost <= statusLost) {
+      alert("Вы выиграли");
+      reset()
+    
+  } else if (lost > statusLost) {
+      alert("Вы проиграли");
+      reset()
+   
   }
+}
+function reset() {
+    dead = 0;
+    lost = 0;
+    document.querySelector("#dead").textContent = dead;
+    document.querySelector("#lost").textContent = lost;
 }
