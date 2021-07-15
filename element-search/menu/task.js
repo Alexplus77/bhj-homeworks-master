e7a000fd0d4d35c76b282e7c85676a7d05b0e8da
Alexplus77 <alexplus77@mail.ru>
2021-07-15 17:32:09 +0200

    hhh

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,21 +1,21 @@
 const menu_subParent = document.querySelector(".menu_sub").parentElement
-const menu_sub = document.querySelector(".menu_sub")
+const menu_sub = document.querySelector(".menu .menu_sub")
 const menu_subItem = menu_sub.querySelector('.menu__item')
-const menuItemParent = document.querySelector(".menu__item").parentElement;
+const menuItemParent = document.querySelector(".menu");
 const link = document.querySelector("a");
 
 //console.log(menu_subItem);
-console.log(menuItemParent);
+console.log(link);
 const menuItem = (event) => {
-  console.log(event.target);  
-     
-        if (menu_sub.className !== "menu_active") {
-    if (event.target.classList.contains("menu__link")) {
-      menu_sub.className = "menu_active";
+  console.log(event.target);       
+        
+    if (event.target.classList.contains("menu__link") && !menu_sub.className.includes("menu_active" )) {
+      menu_sub.className = "menu menu_sub menu_active";
             }
             
-       } else {
-    menu_sub.className = "menu_sub";
+        else {
+        menu_sub.className = "menu_sub";
+        return
     }
     if (event.target.tagName === "A") {
        event.preventDefault();
