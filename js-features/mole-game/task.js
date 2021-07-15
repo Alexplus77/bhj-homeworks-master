00db8f3e9877abb6e6cb9298b46002885de9f4ef
Alexplus77 <alexplus77@mail.ru>
2021-07-15 09:46:50 +0200

    hhh

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,30 +4,33 @@ let dead = 0;
 const statusWin = 10;
 const statusLost = 5;
 const findHole = (event) => {
+    
   if (event.target.classList.contains("hole_has-mole")) {
     document.querySelector("#dead").textContent = ++dead;
   } else {
     document.querySelector("#lost").textContent = ++lost;
   }
-  win();  
+  win();
 };
 
 holeAll.addEventListener("click", findHole);
 
+const displeyWin = () => setTimeout(() => alert(dead === statusWin ? 'Вы выиграли' : 'Вы проиграли'), 10)
+
 function win() {
   if (dead === statusWin && lost <= statusLost) {
-      alert("Вы выиграли");
-      reset()
-    
+    displeyWin()
+    reset();
   } else if (lost > statusLost) {
-      alert("Вы проиграли");
-      reset()
-   
+    displeyWin()
+    reset();
   }
 }
 function reset() {
-    dead = 0;
-    lost = 0;
-    document.querySelector("#dead").textContent = dead;
-    document.querySelector("#lost").textContent = lost;
+    setTimeout(() => {
+       dead = 0;
+  lost = 0;
+  document.querySelector("#dead").textContent = dead;
+  document.querySelector("#lost").textContent = lost; 
+    },20)  
 }
