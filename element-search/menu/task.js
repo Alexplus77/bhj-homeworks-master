e6aa04ba093dba90abca7866c85314a6af140727
Alexplus77 <alexplus77@mail.ru>
2021-07-15 16:48:12 +0200

    jjjj

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,21 +1,29 @@
-const menu_sub = document.querySelector(".menu_sub");
-const menu = document.querySelector(".menu__item").parentElement
+const menu_subParent = document.querySelector(".menu_sub").parentElement
+const menu_sub = document.querySelector(".menu_sub")
+const menu_subItem = menu_sub.querySelector('.menu__item')
+const menuItemParent = document.querySelector(".menu__item").parentElement;
 const link = document.querySelector("a");
 
-
-console.log();
-//console.log(menu);
-const menuItem = (event) => { 
-    
-    console.log(event.target.classList);
-  if (event.target.classList.contains("menu__link")) {
-    
-    menu_sub.className = "menu_active";
+//console.log(menu_subItem);
+console.log(menuItemParent);
+const menuItem = (event) => {
+  console.log(event.target);  
+     
+        if (menu_sub.className !== "menu_active") {
+    if (event.target.classList.contains("menu__link")) {
+      menu_sub.className = "menu_active";
+            }
+            
+       } else {
+    menu_sub.className = "menu_sub";
     }
-   if (event.target.tagName === 'A') {
-        event.preventDefault();      
-    } 
+    if (event.target.tagName === "A") {
+       event.preventDefault();
+    }  
+  
 };
+menuItemParent.addEventListener("click", menuItem);
 
 
-menu.addEventListener("click", menuItem);
+
+
