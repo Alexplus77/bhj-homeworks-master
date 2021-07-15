c698ce8eb223e21145ddc82ca41a328a5bd4aa74
Alexplus77 <alexplus77@mail.ru>
2021-07-15 19:07:10 +0200

    mmm

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,7 @@
-const menu_subParent = document.querySelector(".menu_sub").parentElement;
+
 const menu_sub = document.querySelector(".menu .menu_sub");
-const menu_subItem = menu_sub.querySelector(".menu__item");
 const menuItemParent = document.querySelector(".menu");
-const link = document.querySelector("a");
+
 
 const menuItem = (event) => {
   event.target.classList.contains("menu__link") && notActivMenu(event)
