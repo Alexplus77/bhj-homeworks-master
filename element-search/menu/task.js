9fcac1a99e9c4599b2f3b94887ea88753ce318bf
Alexplus77 <alexplus77@mail.ru>
2021-07-15 19:04:26 +0200

    jjj

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,29 +1,31 @@
-const menu_subParent = document.querySelector(".menu_sub").parentElement
-const menu_sub = document.querySelector(".menu .menu_sub")
-const menu_subItem = menu_sub.querySelector('.menu__item')
+const menu_subParent = document.querySelector(".menu_sub").parentElement;
+const menu_sub = document.querySelector(".menu .menu_sub");
+const menu_subItem = menu_sub.querySelector(".menu__item");
 const menuItemParent = document.querySelector(".menu");
 const link = document.querySelector("a");
 
-//console.log(menu_subItem);
-console.log(link);
 const menuItem = (event) => {
-       
-        
-    if (event.target.classList.contains("menu__link") && !menu_sub.className.includes("menu_active")) {
-        event.target.parentElement.querySelector(".menu_sub").className = "menu menu_sub menu_active"  
-      //menu_sub.className = "menu menu_sub menu_active";
-            }            
-        else {
-        menu_sub.className = "menu menu_sub";
-        
-    }
-    if (event.target.tagName === "A") {
-       event.preventDefault();
-    }  
-  
+  event.target.classList.contains("menu__link") && notActivMenu(event)
+    ? activeMenu(event)
+    : notActivMenu(event);
+
+  if (event.target.tagName === "A") {
+    event.preventDefault();
+  }
 };
+function activeMenu(event) {
+  event.target.parentElement.querySelector(".menu_sub").className =
+    "menu menu_sub menu_active";
+}
+function notActivMenu(event) {
+  if (
+    !event.target.parentElement
+      .querySelector(".menu_sub")
+      .className.includes("menu_active")
+  ) {
+    return true;
+  }
+  event.target.parentElement.querySelector(".menu_sub").className =
+    "menu menu_sub";
+}
 menuItemParent.addEventListener("click", menuItem);
-
-
-
-
