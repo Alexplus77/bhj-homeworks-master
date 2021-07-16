69027d0203fa607ef50f38b8848090347e0f5581
Alexplus77 <alexplus77@mail.ru>
2021-07-16 06:49:14 +0200

    ddd

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,27 +4,52 @@ const menuItemParent = document.querySelector(".menu");
 
 
 const menuItem = (event) => {
-  event.target.classList.contains("menu__link") && notActivMenu(event)
+  
+  linkBlock(event)
+  
+  event.target.classList.contains("menu__link") && notActivMenu(event) 
     ? activeMenu(event)
     : notActivMenu(event);
-
-  if (event.target.tagName === "A") {
-    event.preventDefault();
-  }
+ 
+    
+     if (event.target.tagName === "A") {
+       event.preventDefault();
+     }
+    
 };
 function activeMenu(event) {
+    
+    if (
+      event.target.parentElement
+        .querySelector(".menu_sub")
+        .className.includes("menu_active")
+    ) {
+      return true;
+    }
   event.target.parentElement.querySelector(".menu_sub").className =
-    "menu menu_sub menu_active";
+        "menu menu_sub menu_active";    
+    
 }
 function notActivMenu(event) {
   if (
     !event.target.parentElement
       .querySelector(".menu_sub")
-      .className.includes("menu_active")
+      .className.includes("menu_active") 
   ) {
     return true;
   }
   event.target.parentElement.querySelector(".menu_sub").className =
     "menu menu_sub";
+}
+
+function linkBlock(event) {
+  if (
+    event.target.parentElement
+      .querySelector(".menu_sub")
+      .querySelector(".menu__item")
+      .querySelector(".menu__link").className.includes('menu__link')
+  ) {
+    return false
+  }
 }
 menuItemParent.addEventListener("click", menuItem);
