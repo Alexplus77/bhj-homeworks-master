048cc89e6bbeafda5f806e909d7c7cd60479577e
Alexplus77 <alexplus77@mail.ru>
2021-07-17 13:50:42 +0200

    рррр

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,28 +2,25 @@ const menuMain = document.querySelector(".menu_main");
 const [...menuSub] = document.querySelectorAll(".menu_sub");
 
 function displaySubMenu(event) {
-  
   if (event.target.nextElementSibling.classList.contains("menu_sub")) {
     event.preventDefault();
     menuSub.forEach((elem) => {
       if (elem === event.target.nextElementSibling) {
-      return     
+        return;
       }
-      elem.classList.remove("menu_active")
-  });
-    event.target.nextElementSibling.classList.toggle("menu_active");    
+      elem.classList.remove("menu_active");
+    });
+    event.target.nextElementSibling.classList.toggle("menu_active");
   }
-  
 }
 
 function hideSubMenu(event) {
-  if (event.target.closest(".menu__item")) {
-    return;
-  } 
+  if (!event.target.closest(".menu__item")) {
     menuSub.forEach((elem) => {
       elem.classList.remove("menu_active");
     });
   }
+}
 
 menuMain.addEventListener("click", displaySubMenu);
 document.addEventListener("click", hideSubMenu);
