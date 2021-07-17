17011f9ed21a7853e4ff83511429d945f76ddb76
Alexplus77 <alexplus77@mail.ru>
2021-07-17 11:20:23 +0200

    ffff

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,11 +2,17 @@ const menuMain = document.querySelector(".menu_main");
 const [...menuSub] = document.querySelectorAll(".menu_sub");
 
 function displaySubMenu(event) {
+  menuSub.forEach((elem) => {
+    if (elem.classList.contains("menu_active")) {
+      elem.classList.remove("menu_active");
+    }
+  });
   if (event.target.nextElementSibling.classList.contains("menu_sub")) {
     event.preventDefault();
     event.target.nextElementSibling.classList.toggle("menu_active");
   }
 }
+
 function hideSubMenu(event) {
   if (event.target.closest(".menu__item")) {
     return;
