21c5adfd61985b44e289e6ffbc5ec16d8b1e2eec
Alexplus77 <alexplus77@mail.ru>
2021-07-19 17:38:07 +0200

    llll

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,9 +1,9 @@
 const modalMain = document.querySelector("#modal_main");
+const modalAll = document.querySelectorAll(".modal");
 const succesPopup = document.querySelector("#modal_success");
 
 function openPopup(event) {
   modalMain.classList.toggle("modal_active");
-  console.log(event.target);
   if (event.target.classList.contains("show-success")) {
     succesPopup.classList.add("modal_active");
   }
@@ -12,7 +12,9 @@ function openPopup(event) {
 
 function closePopup(event) {
   if (event.target.classList.contains("modal__close_times")) {
-    event.target.closest(".modal").classList.remove("modal_active");
+    modalAll.forEach((elem) => {
+      elem.classList.remove("modal_active");
+    });
   }
 }
 document.addEventListener("click", openPopup);
