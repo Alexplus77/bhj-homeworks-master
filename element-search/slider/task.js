84d3ff82ebb64593c01f1f544265d460c60cbc62
Alexplus77 <alexplus77@mail.ru>
2021-07-21 12:36:18 +0200

    kkk

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,33 +1,43 @@
 const slider = document.querySelector('.slider__items')
 const [...sliderItems] = document.querySelectorAll(".slider__item");
 const sliderArrow = document.querySelector(".slider__arrows");
+const [...sliderDots] = document.querySelectorAll(".slider__dot");
 
 let index = 0
-function show(event) {
-  sliderItems.forEach(elem => {
+function show(event) {  
+  console.log(sliderItems); 
+  validatePic(event)  
+   sliderItems.forEach(elem => {
     elem.classList.remove('slider__item_active')
   })
-  console.log(sliderItems);
+  sliderDots.forEach((elem) => {
+    elem.classList.remove("slider__dot_active");
+  });
+  sliderItems[index].classList.add("slider__item_active");
+  sliderDots[index].classList.add("slider__dot_active");
   
-    sliderItems[index].classList.add("slider__item_active")
-     if ( event.target.classList.contains("slider__arrow_next") && index>=0) {
-      index++
-        if ( index === sliderItems.length) {        
-            index=0           
-        }   
-    }   
-    if (event.target.classList.contains("slider__arrow_prev")) {
-      index--
-      if ( index === 0) {        
-        index = sliderItems.length
-        
-        }   
-    }     
-   
 }
 
 function validatePic(event) {
-  
+  if (event.target.classList.contains("slider__arrow_next")) {   
+     
+        if ( index+1 < sliderItems.length) {        
+            index++          
+        } else if (index + 1 >= sliderItems.length) {
+          index=0
+    }    
+  }
+
+  if (event.target.classList.contains("slider__arrow_prev")) {
+
+     if (index === 0) {
+         index = sliderItems.length;
+     } else  {
+       index--;
+     }
+
+    console.log(index)
+  }
 }
 
 sliderArrow.addEventListener('click', show)
