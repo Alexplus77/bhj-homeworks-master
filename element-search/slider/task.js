17aaf33b778d04218dc67d5c1d54a419f9ec1902
Alexplus77 <alexplus77@mail.ru>
2021-07-21 14:25:54 +0200

    jjj

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,38 @@
-
-
 const sliderNext = document.querySelector(".slider__arrow_next");
 const sliderPrev = document.querySelector(".slider__arrow_prev");
 const [...sliderItems] = document.querySelectorAll(".slider__item");
 const [...sliderDots] = document.querySelectorAll(".slider__dot");
 
-let index = 0
+let index = 0;
 function show() {
-  sliderItems.forEach(elem => {
-  elem.classList.remove("slider__item_active");
-  })
-   sliderDots.forEach((elem) => {
-     elem.classList.remove("slider__dot_active");
-   });
+  sliderItems.forEach((elem) => {
+    elem.classList.remove("slider__item_active");
+  });
+  sliderDots.forEach((elem) => {
+    elem.classList.remove("slider__dot_active");
+  });
   sliderItems[index].classList.add("slider__item_active");
   sliderDots[index].classList.add("slider__dot_active");
-  console.log(index)
+  console.log(index);
 }
 
 function setNext() {
-  show()
-  index++
+  show();
+  index++;
   if (index >= sliderItems.length) {
-    index=0
+    index = 0;
   }
 }
 
 function setPrev() {
   show();
   index--;
-  if (index+1 <= 0) {
-    index = sliderItems.length-1
-  } else {index}
+  if (index + 1 <= 0) {
+    index = sliderItems.length - 1;
+  } else {
+    index;
+  }
 }
 
-sliderNext.addEventListener('click', setNext)
+sliderNext.addEventListener("click", setNext);
 sliderPrev.addEventListener("click", setPrev);
