754a47ac62edf6e685cfeaba774ea96abe21dd88
Alexplus77 <alexplus77@mail.ru>
2021-07-23 16:30:49 +0200

    ddd

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,22 +1,17 @@
 const menuAll = [...document.querySelectorAll(".tab")];
 const contentAll = [...document.querySelectorAll(".tab__content")];
 
-
 function setActiveTab(e) {
-    
-  closePrevTab(index);
+  closePrevTab();
+  let index = menuAll.indexOf(e.target);
   e.target.classList.add("tab_active");
-
- const index = menuAll.findIndex((elem) => elem.classList.contains("tab_active"));
-  contentAll[index].classList.add("tab__content_active");
+    contentAll[index].classList.add("tab__content_active");
+    
 }
 
-function closePrevTab(index) {
+function closePrevTab() {
+  menuAll.forEach((elem) => elem.classList.remove("tab_active"));
 
-  menuAll.forEach((elem) => {
-    elem.classList.remove("tab_active");
-    contentAll[index].classList.remove("tab__content_active");
-  });
-  
+  contentAll.forEach((elem) => elem.classList.remove("tab__content_active"));
 }
 menuAll.forEach((elem) => elem.addEventListener("click", setActiveTab));
