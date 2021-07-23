89b74e4e0d531fe79b555a1b33ac1d1bbabbd83a
Alexplus77 <alexplus77@mail.ru>
2021-07-23 12:14:13 +0200

    fffff

---
+++ event-object/tabs/task.js
@@ -0,0 +1,23 @@
+const [...menuAll] = document.querySelectorAll(".tab");
+const [...contentAll] = document.querySelectorAll(".tab__content");
+const navMenu = document.querySelector(".tab__navigation");
+
+let index = 0;
+
+function openMenuContent(e) {
+  closeMenuContent();
+
+  e.target.classList.add("tab_active");
+
+  index = menuAll.findIndex((elem) => elem.classList.contains("tab_active"));
+  contentAll[index].classList.add("tab__content_active");
+}
+
+function closeMenuContent() {
+  menuAll.forEach((elem) => {
+    elem.classList.remove("tab_active");
+    contentAll[index].classList.remove("tab__content_active");
+  });
+}
+
+navMenu.addEventListener("click", openMenuContent);
