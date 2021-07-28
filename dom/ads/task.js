2547295055e826b78eb078467462fac68560941a
Alexplus77 <alexplus77@mail.ru>
2021-07-28 13:41:51 +0200

    hhhh

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,27 +1,27 @@
 const caseRotator = [...document.querySelectorAll(".rotator__case")];
 
-  function rotator() {
-    let index = caseRotator.findIndex((elem) =>
-      elem.classList.contains("rotator__case_active")
-    );
-    removeCase();
-    index++;
-    if (index === caseRotator.length) {
-      index = 0;
-    }
-    caseRotator[index].classList.add("rotator__case_active");
-    caseRotator[index].style.color = caseRotator[index].dataset['color']
-    let time=caseRotator[index].dataset["speed"]
-    console.log(time)
+
+
+function removeCase() {
+  caseRotator.forEach((elem) => elem.classList.remove("rotator__case_active"));
 } 
+  
 
- 
-  function removeCase() {
-    caseRotator.forEach((elem) => elem.classList.remove("rotator__case_active"));
+
+
+function setCount(elem) {
+  
+  return () => {
+    
+     elem.classList.add("rotator__case_active");
   }
-  ;
-setInterval(rotator, 1000);
+}
 
 
+caseRotator.forEach((elem) => {  
+  let count = setCount(elem);
+  let time = elem.dataset["speed"];
 
-
+  console.log(elem);  
+  setInterval(count, time);
+})
