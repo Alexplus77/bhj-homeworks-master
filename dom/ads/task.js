f1e2a0bcf7fd97ed9284ab9af0e474bf15f1d57c
Alexplus77 <alexplus77@mail.ru>
2021-07-28 13:46:36 +0200

    sss

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,27 +1,19 @@
 const caseRotator = [...document.querySelectorAll(".rotator__case")];
 
-
-
 function removeCase() {
   caseRotator.forEach((elem) => elem.classList.remove("rotator__case_active"));
-} 
-  
-
-
-
-function setCount(elem) {
-  
-  return () => {
-    
-     elem.classList.add("rotator__case_active");
-  }
 }
 
+function setCount(elem) {
+  return () => {
+    elem.classList.add("rotator__case_active");
+  };
+}
 
-caseRotator.forEach((elem) => {  
+caseRotator.forEach((elem) => {
   let count = setCount(elem);
   let time = elem.dataset["speed"];
 
-  console.log(elem);  
+  console.log(elem);
   setInterval(count, time);
-})
+});
