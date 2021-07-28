d0180ff0c7667d05e895b51471caaea40c61acee
Alexplus77 <alexplus77@mail.ru>
2021-07-28 18:14:01 +0200

    ffff

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,39 +1,31 @@
-const caseRotator1 = [...document.querySelector(".rotator1").children];
-const caseRotator2 = [...document.querySelector(".rotator2").children];
-const caseRotator3 = [...document.querySelector(".rotator3").children];
-const caseRotator4 = [...document.querySelector(".rotator4").children];
-const caseRotator5 = [...document.querySelector(".rotator5").children];
-const caseRotator6 = [...document.querySelector(".rotator6").children];
+function rotator(rotatorItem) {
+  let rotatorSlides = [...document.querySelector(rotatorItem).children];
 
-
-function rotator(arr) {
+  let index = 0;
   return () => {
-    let index = arr.findIndex((elem) =>
-      elem.classList.contains("rotator__case_active")
-    );
-    removeCase(arr);
+    removeCase(rotatorSlides, index);
     index++;
-    if (index === arr.length) {
+    if (index === rotatorSlides.length) {
       index = 0;
     }
-    arr[index].classList.add("rotator__case_active");
-    arr[index].style.color = arr[index].dataset["color"];
+    rotatorSlides[index].classList.add("rotator__case_active");
+    rotatorSlides[index].style.color = rotatorSlides[index].dataset["color"];
   };
 }
 
-function removeCase(arr) {
-  arr.forEach((elem) => elem.classList.remove("rotator__case_active"));
+function removeCase(rotatorSlides, index) {
+  rotatorSlides[index].classList.remove("rotator__case_active");
 }
 
-let case1 = rotator(caseRotator1);
+let case1 = rotator(".rotator1");
 setInterval(case1, 1000);
-let case2 = rotator(caseRotator2);
+let case2 = rotator(".rotator2");
 setInterval(case2, 2000);
-let case3 = rotator(caseRotator3);
+let case3 = rotator(".rotator3");
 setInterval(case3, 3000);
-let case4 = rotator(caseRotator4);
+let case4 = rotator(".rotator4");
 setInterval(case4, 4000);
-let case5 = rotator(caseRotator5);
+let case5 = rotator(".rotator5");
 setInterval(case5, 5000);
-let case6 = rotator(caseRotator6);
+let case6 = rotator(".rotator6");
 setInterval(case6, 6000);
