536fdb3d63b4676611e1772c3d0de3c08f8b36f3
Alexplus77 <alexplus77@mail.ru>
2021-07-29 14:45:31 +0200

    ffff

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,21 +1,68 @@
-const fontSize = [...document.querySelectorAll(".font-size")];
+const fontSizeText = [...document.querySelector(".book__control_font-size").children];
+const textColor = [...document.querySelector(".book__control_color").children];
+const backgColor = [...document.querySelector(".book__control_background").children];
 const textContent = document.querySelector(".book__content");
 
-const changeSize = () => {
+const setChange = (selector, activate, changeItem) => {
+  const menuItem = [...document.querySelector(selector).children];
   return (e) => {
     e.preventDefault();
-    reset("font-size_active");
-    e.target.classList.add("font-size_active");
-    textContent.style.fontSize = getComputedStyle(e.target).fontSize;
+    reset(activate, menuItem);
+      e.target.classList.add(activate);
+      console.log(changeItem);
+    changeElem(changeItem,e)
   };
 };
 
-const reset = (active) => {
-  fontSize.forEach((elem) => {
+const changeElem = (changeItem, e) => {
+    if (changeItem === 'fontSize') { textContent.style.fontSize = getComputedStyle(e.target).fontSize; }
+    if (changeItem === "color") { textContent.style.color = e.target.dataset["textColor"]; }
+    if (changeItem === "backgroundColor") { textContent.closest(".book").style.backgroundColor =
+      e.target.dataset["bgColor"];
+    }
+    
+}
+const reset = (active, menuItem) => {
+  menuItem.forEach((elem) => {
     elem.classList.remove(active);
   });
 };
 
-let font = changeSize();
+let font = setChange(".book__control_font-size", "font-size_active", 'fontSize');
+let text = setChange(".book__control_color", "color_active", 'color');
+let bgColor = setChange(".book__control_background", "color_active", 'backgroundColor');
 
-fontSize.forEach((elem) => elem.addEventListener("click", font));
+fontSizeText.forEach((elem) => elem.addEventListener("click", font));
+textColor.forEach((elem) => elem.addEventListener("click", text));
+backgColor.forEach((elem) => elem.addEventListener("click", bgColor));
+
+
+
+// let text = changeTextColor(".book__control_color", "color_active", "color");
+// let bgColor = changeBgColor(
+//   ".book__control_background",
+//   "color_active",
+//   "backgroundColor"
+// );
+
+
+
+// const changeTextColor = (selector, activate) => {
+//   const menuItem = [...document.querySelector(selector).children];
+//   return (e) => {
+//     e.preventDefault();
+//     reset(activate, menuItem);
+//     e.target.classList.add(activate);
+//     textContent.style.color = e.target.dataset["textColor"];
+//   };
+// };
+
+// const changeBgColor = (selector, activate) => {
+//   const menuItem = [...document.querySelector(selector).children];
+//   return (e) => {
+//     e.preventDefault();
+//     reset(activate, menuItem);
+//     e.target.classList.add(activate);
+//     textContent.closest(".book").style.backgroundColor = e.target.dataset["bgColor"];
+//   };
+// };
