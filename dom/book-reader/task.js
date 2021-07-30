2fe64b441a5f9593e21c4bb76ef8d8d55fb9f423
Alexplus77 <alexplus77@mail.ru>
2021-07-30 07:17:56 +0200

    с обьктом objElemStyle возникают баги с размером шрифта, перезписывается ключь fontSize на 16px.

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,47 +1,41 @@
 const textContent = document.querySelector(".book__content");
 
 const setChange = (selector, activate, changeItem) => {
-    const menuItem = [...document.querySelectorAll(selector)];
-    
+  const menuItem = [...document.querySelectorAll(selector)];
   return (e) => {
     e.preventDefault();
     reset(activate, menuItem);
     e.target.classList.add(activate);
-      changeElemStyle(changeItem, e);
-      
+    changeElemStyle(changeItem, e);
   };
 };
 
 const changeElemStyle = (changeItem, e) => {
-    if (changeItem === "fontSize") {
-        textContent.style.fontSize = getComputedStyle(e.target).fontSize;
-    }
-    if (changeItem === "color") {
-        textContent.style.color = e.target.dataset["textColor"];
-    }
-    if (changeItem === "backgroundColor") {
-        textContent.closest(".book").style.backgroundColor =
-            e.target.dataset["bgColor"];
-    }
-}
+  switch (changeItem) {
+    case "fontSize":
+      textContent.style.fontSize = getComputedStyle(e.target).fontSize;
+      break;
+    case "color":
+      textContent.style.color = e.target.dataset["textColor"];
+      break;
+    case "backgroundColor":
+      textContent.closest(".book").style.backgroundColor =
+        e.target.dataset["bgColor"];
+    default:
+      break;
+  }
+};
+
 const reset = (active, menuItem) => {
   menuItem.forEach((elem) => {
     elem.classList.remove(active);
   });
 };
 
-let fontSize = setChange(
-  ".font-size",
-  "font-size_active",
-  "fontSize"
-);
+let fontSize = setChange(".font-size", "font-size_active", "fontSize");
 let textColor = setChange(".color", "color_active", "color");
 
-let bgColor = setChange(
-  ".color",
-  "color_active",
-  "backgroundColor"
-);
+let bgColor = setChange(".color", "color_active", "backgroundColor");
 
 [...document.querySelectorAll(".font-size")].forEach((elem) =>
   elem.addEventListener("click", fontSize)
@@ -52,8 +46,6 @@ let bgColor = setChange(
 [...document.querySelectorAll(".color")].forEach((elem) =>
   elem.addEventListener("click", bgColor)
 );
-
-
 
 // const objElemStyle = {
 //   fontSize: (textContent.style.fontSize = getComputedStyle(e.target).fontSize),
