3281d34346c9ad64750e3404216b6897629106f0
Alexplus77 <alexplus77@mail.ru>
2021-08-18 15:55:33 +0200

    ыыы

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -1,31 +1,30 @@
 class Autocomplete {
-  constructor( container ) {
+  constructor(container) {
     this.container = container;
-    this.input = container.querySelector( '.autocomplete__input' );
-    this.searchInput = container.querySelector( '.autocomplete__search' );
-    this.list = container.querySelector( '.autocomplete__list' );
-    this.valueContainer = container.querySelector( '.autocomplete__value' );
-    this.valueElement = container.querySelector( '.autocomplete__text-content' );
+    this.input = container.querySelector(".autocomplete__input");
+    this.searchInput = container.querySelector(".autocomplete__search");
+    this.list = container.querySelector(".autocomplete__list");
+    this.valueContainer = container.querySelector(".autocomplete__value");
+    this.valueElement = container.querySelector(".autocomplete__text-content");
 
     this.registerEvents();
   }
 
   registerEvents() {
-    this.valueContainer.addEventListener( 'click', e => {
-      this.searchInput.classList.add( 'autocomplete__search_active' );
-      this.list.classList.add( 'autocomplete__list_active' );
+    this.valueContainer.addEventListener("click", (e) => {
+      this.searchInput.classList.add("autocomplete__search_active");
+      this.list.classList.add("autocomplete__list_active");
       this.searchInput.value = this.valueElement.textContent.trim();
       this.searchInput.focus();
 
       this.onSearch();
     });
 
+    this.searchInput.addEventListener("input", (e) => this.onSearch());
 
-    this.searchInput.addEventListener( 'input', e => this.onSearch());
-
-    this.list.addEventListener( 'click', e => {
+    this.list.addEventListener("click", (e) => {
       const { target } = e;
-      if ( !target.matches( '.autocomplete__item' )) {
+      if (!target.matches(".autocomplete__item")) {
         return;
       }
 
@@ -35,39 +34,41 @@ class Autocomplete {
       this.onSelect({
         index,
         text,
-        value
+        value,
       });
     });
   }
 
-  onSelect( item ) {
+  onSelect(item) {
     this.input.selectedIndex = item.index;
     this.valueElement.textContent = item.text;
 
-    this.searchInput.classList.remove( 'autocomplete__search_active' );
-    this.list.classList.remove( 'autocomplete__list_active' );
+    this.searchInput.classList.remove("autocomplete__search_active");
+    this.list.classList.remove("autocomplete__list_active");
   }
 
   onSearch() {
-    const matches = this.getMatches( this.searchInput.value );
+    const matches = this.getMatches(this.searchInput.value);
 
-    this.renderMatches( matches );
+    this.renderMatches(matches);
   }
 
-  renderMatches( matches ) {
-    const html = matches.map( item => `
+  renderMatches(matches) {
+    const html = matches.map(
+      (item) => `
     	<li>
         <span class="autocomplete__item"
         	data-index="${item.index}"
           data-id="${item.value}"
         >${item.text}</span>
       </li>
-    `);
+    `
+    );
 
-    this.list.innerHTML = html.join('');
+    this.list.innerHTML = html.join("");
   }
 
-  getMatches( text ) {
+  getMatches(text) {
     /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
@@ -81,23 +82,17 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    
- return  [...this.input.options].reduce((acc, elem, i) => {
-     console.log(text.split(''))
-     if (elem.text.split('').includes(text.split('')[i])) {
-       acc.push({ text: elem.text, value: elem.value })
-       
-     }
-     return acc
-   },[])
-       
-    // return [
-    //   {
-    //     text: 'Чубакка',
-    //     value: '1'
-    //   }
-    // ];
+
+    return [...this.input.options].reduce((acc, elem, i) => {
+      console.log(text.split(""));
+      if (elem.text.split("").includes(text.split("")[i])) {
+        acc.push({ text: elem.text, value: elem.value });
+      }
+      return acc;
+    }, []);
+
+   
   }
 }
 
-new Autocomplete( document.querySelector( '.autocomplete' ));
+new Autocomplete(document.querySelector(".autocomplete"));
