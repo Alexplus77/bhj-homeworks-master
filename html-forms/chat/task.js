33720d4ad4cf881fc7c5280f889347938cd78e6b
Alexplus77 <alexplus77@mail.ru>
2021-08-21 12:02:03 +0200

    hhhh

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -2,40 +2,52 @@ const widjet = document.querySelector(".chat-widget");
 const inputClient = document.querySelector(".chat-widget__input");
 const chatMessage = document.querySelector(".chat-widget__messages");
 
-const widjetOpen = () => {  widjet.classList.add("chat-widget_active")};
-const realTime = () => { return new Date().toLocaleTimeString().slice(0, -3)};
+const widjetOpen = () => {
+  widjet.classList.add("chat-widget_active");
+};
+const realTime = () => {
+  return new Date().toLocaleTimeString().slice(0, -3);
+};
 
-const messageClient = () => {
-  console.log(inputClient.value)
-  if(inputClient.value===''){inputClient.value='Привет'}
+const markupMessage = (text) => {
   chatMessage.innerHTML += `
-      <div class="message message_client">
+      <div class="message ">
         <div class="message__time">${realTime()}</div>
         <div class="message__text">
-        ${inputClient.value}
+        ${text}
         </div>
       </div>`;
+};
+
+const messageClient = () => {
+  markupMessage(inputClient.value);
+  const message = document.querySelector(".message");
+  message.classList.add("message_client");
+  message.scrollIntoView();
   messageRobot();
   inputClient.value = "";
 };
 
 const messageRobot = () => {
-  const robotWords = ['Здравствуйте', 'заказ обрабатывается', 'до свидания', 'как дела' ]  
+  const robotWords = [
+    "Здравствуйте",
+    "заказ обрабатывается",
+    "до свидания",
+    "как дела",
+  ];
   const index = Math.floor(Math.random() * robotWords.length);
-  chatMessage.innerHTML += `
-      <div class="message">
-        <div class="message__time">${realTime()}</div>
-        <div class="message__text">
-         ${robotWords[index]}
-        </div>
-      </div>`;
+  markupMessage(robotWords[index]);
+};
+
+const waitingClient = () => {
+  setTimeout(messageRobot, 30000);
 };
 
 widjet.addEventListener("click", widjetOpen);
 inputClient.addEventListener("change", messageClient);
-
+inputClient.addEventListener("blur", waitingClient);
 // document.addEventListener('click', (e) => {
 //     if(e.target != widjet ){
 //     { widjet.classList.remove("chat-widget_active") }};
-  
+
 // })
