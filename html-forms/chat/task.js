db30476ee5f85bb464f36e9b2b972da6c32f48e5
Alexplus77 <alexplus77@mail.ru>
2021-08-26 12:49:11 +0200

    hhh

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,8 +1,13 @@
 const widjet = document.querySelector(".chat-widget");
 const inputClient = document.querySelector(".chat-widget__input");
 const chatMessage = document.querySelector(".chat-widget__messages");
+const icon = document.querySelector(".fas ")
 
 const widjetOpen = () => widjet.classList.add("chat-widget_active");
+const widjetClose = (e) => {
+  console.log(e.target, widjet)
+  widjet.classList.remove("chat-widget_active")
+};
 const realTime = () => new Date().toLocaleTimeString().slice(0, -3);
 
 // const markupMessage = (text) => {
@@ -54,8 +59,5 @@ const waitingClient = () => setTimeout(messageRobot, 30000);
 widjet.addEventListener("click", widjetOpen);
 inputClient.addEventListener("change", messageClient);
 inputClient.addEventListener("blur", waitingClient);
-// document.addEventListener('click', (e) => {
-//     if(e.target != widjet ){
-//     { widjet.classList.remove("chat-widget_active") }};
+icon.addEventListener("click", widjetClose);
 
-// })
