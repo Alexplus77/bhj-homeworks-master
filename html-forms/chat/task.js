ba241d50ef70db143123878ea1b9c6183f4d3213
Alexplus77 <alexplus77@mail.ru>
2021-08-26 11:48:21 +0200

    dfdfd

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -5,24 +5,37 @@ const chatMessage = document.querySelector(".chat-widget__messages");
 const widjetOpen = () => widjet.classList.add("chat-widget_active");
 const realTime = () => new Date().toLocaleTimeString().slice(0, -3);
 
-const markupMessage = (text) => {
-  chatMessage.innerHTML += `
-      <div class="message ">
-        <div class="message__time">${realTime()}</div>
-        <div class="message__text">
-        ${text}
-        </div>
-      </div>`;  
+// const markupMessage = (text) => {
+//   chatMessage.innerHTML += `
+//       <div class="message ">
+//         <div class="message__time">${realTime()}</div>
+//         <div class="message__text">
+//         ${text}
+//         </div>
+//       </div>`;
+// };
+const generateMarkup = (text, className = "message") => {
+  const messageContainer = document.createElement("div");
+  messageContainer.classList.add(...className);
+  const messageTime = document.createElement("div");
+  messageTime.classList.add("message__time");
+  messageTime.innerHTML = realTime();
+  const messageText = document.createElement("div");
+  messageText.classList.add("message__text");
+  messageText.innerHTML = text;
+  messageContainer.appendChild(messageTime);
+  messageContainer.appendChild(messageText);
+  chatMessage.appendChild(messageContainer);
+  messageContainer.scrollIntoView(true);
 };
 
 const messageClient = () => {
-  markupMessage(inputClient.value);
+  generateMarkup(inputClient.value);
   const message = document.querySelector(".message");
   message.classList.add("message_client");
-  message.scrollIntoView()
-  messageRobot();  
+
+  messageRobot();
   inputClient.value = "";
-  
 };
 
 const messageRobot = () => {
@@ -33,11 +46,10 @@ const messageRobot = () => {
     "как дела",
   ];
   const index = Math.floor(Math.random() * robotWords.length);
-  markupMessage(robotWords[index]);
+   generateMarkup(robotWords[index]);
 };
 
 const waitingClient = () => setTimeout(messageRobot, 30000);
-
 
 widjet.addEventListener("click", widjetOpen);
 inputClient.addEventListener("change", messageClient);
