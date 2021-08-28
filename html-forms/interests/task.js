bf83778203e0d43c1aa5d8d0d8486bf07079e66d
Alexplus77 <alexplus77@mail.ru>
2021-08-28 17:29:54 +0200

    hhhh

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,40 +1,59 @@
 const findNodeAll = (selector, parent = document) => [
   ...parent.querySelectorAll(selector),
 ];
-const findNodeOne = (selector, parent = document) => parent.querySelector(selector);
+const findNodeOne = (selector, parent = document) =>
+  parent.querySelector(selector);
 const interestsItem = findNodeAll(".interests");
-
 
 const childrenCheck = (children, parents) => {
   children.forEach((child) => {
-      child.addEventListener("change", () => {
-        if (children.every((elem) => elem.checked)) {
-          findNodeOne('input', parents).indeterminate = false
-           findNodeOne("input", parents).checked = true;
-           }
-    else {
-          findNodeOne("input", parents).indeterminate = true;
-          findNodeOne("input", parents).checked = false;
-        }  
-           
-      });
+    child.addEventListener("change", () => {
+      if (children.every((elem) => elem.checked)) {
+        findNodeOne("input", parents).indeterminate = false;
+        findNodeOne("input", parents).checked = true;
+      } else {
+        findNodeOne("input", parents).indeterminate = true;
+        findNodeOne("input", parents).checked = false;
+      }
     });
-}
+  });
+};
 
 parentsCheck = (children, parents) => {
   parents.addEventListener("change", (e) => {
-      e.target.checked
-        ? children.forEach((elem) => (elem.checked = true))
-        : children.forEach((elem) => (elem.checked = false));
-    });
-}
+    e.target.checked
+      ? children.forEach((elem) => (elem.checked = true))
+      : children.forEach((elem) => (elem.checked = false));
+  });
+};
 
 interestsItem.forEach((el) => {
   if (el.classList.contains("interests_active")) {
     const parents = el.parentNode.firstElementChild;
     const children = findNodeAll("input", el);
-childrenCheck(children, parents)    
-parentsCheck(children, parents)    
+    childrenCheck(children, parents);
+    parentsCheck(children, parents);
   }
 });
 
+//! Разминка
+
+const makeCamelCase = (str) => {
+  const arrStr = str.split("-");
+  let firstLeter = "";
+  const camelCaseStr = arrStr.map((elem, i) => {
+      const arrLetter = elem.split("");
+      i === 0 ? (firstLeter = elem.split("")[0].toLowerCase()): (firstLeter = elem.split("")[0].toUpperCase());
+      arrLetter.splice(0, 1, firstLeter);
+      return arrLetter.join("");
+  }).join("");
+  
+  return camelCaseStr;
+  
+};
+console.log(makeCamelCase("background-color"));
+makeCamelCase("background-color") // 'backgroundColor';
+makeCamelCase("list-style-image"); //  'listStyleImage';
+makeCamelCase("webkit-transition")  // 'webkitTransition';
+makeCamelCase("border-style-red") // borderStyleRed;
+makeCamelCase("class-class-class-class") // classClassClassClass
