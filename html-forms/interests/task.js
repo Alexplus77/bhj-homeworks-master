989b5c8ce2c7e1ae61d207bf0969ca166ffa72dd
Alexplus77 <alexplus77@mail.ru>
2021-08-28 13:44:57 +0200

    jjjj

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,10 +1,40 @@
-const interestItem = [...document.querySelectorAll(".interest")].forEach(el => {
-        el.addEventListener('change', (e) => {
-        el.querySelectorAll(".interest__check").forEach(elem => {
-       e.target.checked ? (elem.checked = true) : (elem.checked = false);
-        })          
-        
-    })
-})
+const findNodeAll = (selector, parent = document) => [
+  ...parent.querySelectorAll(selector),
+];
+const findNodeOne = (selector, parent = document) => parent.querySelector(selector);
+const interestsItem = findNodeAll(".interests");
 
+
+const childrenCheck = (children, parents) => {
+  children.forEach((child) => {
+      child.addEventListener("change", () => {
+        if (children.every((elem) => elem.checked)) {
+          findNodeOne('input', parents).indeterminate = false
+           findNodeOne("input", parents).checked = true;
+           }
+    else {
+          findNodeOne("input", parents).indeterminate = true;
+          findNodeOne("input", parents).checked = false;
+        }  
+           
+      });
+    });
+}
+
+parentsCheck = (children, parents) => {
+  parents.addEventListener("change", (e) => {
+      e.target.checked
+        ? children.forEach((elem) => (elem.checked = true))
+        : children.forEach((elem) => (elem.checked = false));
+    });
+}
+
+interestsItem.forEach((el) => {
+  if (el.classList.contains("interests_active")) {
+    const parents = el.parentNode.firstElementChild;
+    const children = findNodeAll("input", el);
+childrenCheck(children, parents)    
+parentsCheck(children, parents)    
+  }
+});
 
