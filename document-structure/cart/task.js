e2ad7c24e7fdce366c916a2193b79879cfa35047
Alexplus77 <alexplus77@mail.ru>
2021-08-31 17:47:59 +0200

    fff

---
+++ document-structure/cart/task.js
@@ -0,0 +1,35 @@
+const products = [...document.querySelectorAll(".product")];
+const quantityControl = [...document.querySelectorAll(".product__quantity-controls")];
+const productAddBtn = [...document.querySelectorAll(".product__add")];
+const cartProduct = document.querySelector(".cart__products");
+
+let count = 1;
+const changeQuantity = (e) => {
+  if (e.target.classList.contains("product__quantity-control_dec")) {
+    if (count > 1) {
+      count--;
+    }
+  } else if (e.target.classList.contains("product__quantity-control_inc")) {
+    count++;
+  }
+  e.currentTarget.children[1].innerText = count;
+};
+const arrProdactId = [];
+
+const productAdd = (e) => {
+  products.forEach((product) => {
+      if (product.querySelector(".product__add") === e.target) {
+        if (arrProdactId.includes(product.dataset.id)){return}
+          arrProdactId.push(product.dataset.id);
+      cartProduct.innerHTML += `<div class="cart__product" data-id="${product.dataset.id}">
+                <img class="cart__product-image" src="${product.children[1].src}">
+                <div class="cart__product-count">${count}</div>
+            </div>`;
+      console.log(arrProdactId);
+    }
+  });
+};
+quantityControl.forEach((elem) =>
+  elem.addEventListener("click", changeQuantity)
+);
+productAddBtn.forEach((elem) => elem.addEventListener("click", productAdd));
