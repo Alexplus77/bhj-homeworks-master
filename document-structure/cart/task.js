1db9baa28923425f57d4b4e789ce40ff36283fef
Alexplus77 <alexplus77@mail.ru>
2021-09-01 13:11:28 +0200

    dd

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,41 +1,47 @@
 const products = [...document.querySelectorAll(".product")];
-const quantityControl = [
-  ...document.querySelectorAll(".product__quantity-controls"),
-];
+const quantityControl = [...document.querySelectorAll(".product__quantity-controls")];
 const productAddBtn = [...document.querySelectorAll(".product__add")];
 const cartProduct = document.querySelector(".cart__products");
 
 let count = 1;
 const changeQuantity = (e) => {
+   
   if (e.target.classList.contains("product__quantity-control_dec")) {
     if (count > 1) {
       count--;
     }
   } else if (e.target.classList.contains("product__quantity-control_inc")) {
     count++;
-  }
+    }
+    
   e.currentTarget.children[1].innerText = count;
 };
+const arrProdactId = [];
 
 
 const productAdd = (e) => {
-  products.forEach((product) => {
-    if (product.querySelector(".product__add") === e.target) {
-      const cartProductItem = [
-        ...cartProduct.querySelectorAll(".cart__product"),
-      ];
-      cartProductItem.forEach((elem) => {
-        if (elem.dataset.id === product.dataset.id) {
-          elem.children[1].innerHTML = +elem.children[1].innerHTML + count;
-        }
-      });
-
+    products.forEach((product) => {
+        if (product.querySelector(".product__add") === e.target) {
+            const cartProductItem = [...cartProduct.querySelectorAll(".cart__product")]
+            cartProductItem.forEach(elem => {
+                if (elem.dataset.id === product.dataset.id) {
+                  elem.children[1].innerHTML= +elem.children[1].innerHTML +  count;
+                    console.log();
+                }                  
+            })
+            if (arrProdactId.includes(product.dataset.id)) {              
+                 return
+             
+          } else {
+               arrProdactId.push(product.dataset.id);
       cartProduct.innerHTML += `<div class="cart__product" data-id="${product.dataset.id}">
                 <img class="cart__product-image" src="${product.children[1].src}">
                 <div class="cart__product-count">${count}</div>
             </div>`;
-
-      count = 1;
+          }
+         
+          count=1
+      
     }
   });
 };
