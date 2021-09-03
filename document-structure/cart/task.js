5ad1b6b62e3e96c94fa2fbcf13281ef001e39a08
Alexplus77 <alexplus77@mail.ru>
2021-09-03 18:58:28 +0200

    ddd

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,6 +1,5 @@
-
 import { storeCart, storeShop } from "./store.js";
-console.log(storeShop)
+console.log(storeShop);
 
 const products = [...document.querySelectorAll(".product")];
 const btnDecrement = [
@@ -13,8 +12,8 @@ const productAddBtn = [...document.querySelectorAll(".product__add")];
 const productRemoveBtn = [...document.querySelectorAll(".product__remove")];
 
 const cartProduct = document.querySelector(".cart__products");
-let count=1
-const minNumberOfProducts = 1
+let count = 1;
+const minNumberOfProducts = 1;
 
 const handleDecrement = (e) => {
   if (count > minNumberOfProducts) {
@@ -28,21 +27,20 @@ const handleIncrement = (e) => {
 };
 
 const renderCartHTML = (id, image, count) => {
-  
   const cart = document.createElement("div");
   cart.classList.add("cart__product");
   cart.setAttribute("data-id", id);
-  
+
   const img = document.createElement("img");
   img.classList.add("cart__product-image");
   img.setAttribute("src", image);
   cart.appendChild(img);
-  
+
   const countCart = document.createElement("div");
   countCart.classList.add("cart__product-count");
   countCart.textContent = count;
   cart.appendChild(countCart);
-  
+
   const rm = document.createElement("i");
   rm.classList.add("cart__product-remove");
   rm.classList.add("fas");
@@ -52,41 +50,36 @@ const renderCartHTML = (id, image, count) => {
 };
 
 const saveProductsToCart = (id, image, count) => {
- 
   const findProdactCart = storeCart.find((elem) => elem.id === id);
   if (findProdactCart) {
-     const cartProductAll = [...cartProduct.querySelectorAll(".cart__product")];
-    cartProductAll.forEach((elem) => {     
-        elem.children[2].innerHTML = +elem.children[2].innerHTML + count;      
-    })
+    const cartProductAll = [...cartProduct.querySelectorAll(".cart__product")];
+    cartProductAll.forEach((elem) => {
+      elem.children[2].innerHTML = +elem.children[2].innerHTML + count;
+    });
   } else {
     storeCart.push({
       id: id,
       imageLink: image,
-      count:count
-    })
+      count: count,
+    });
     renderCartHTML(id, image, count);
   }
-  
-  
 };
 
-const addProductToCart = (e,id, img, count) => {
+const addProductToCart = (e, id, img, count) => {
   const targetId = e.target.closest(".product").dataset.id;
-  const findProdact = storeShop.find(elem => elem.id === targetId)
-  const idProduct = findProdact.id
-  const image = findProdact.imageLink  
+  const findProduct = storeShop.find((elem) => elem.id === targetId);
+  const idProduct = findProduct.id;
+  const image = findProduct.imageLink;
   saveProductsToCart(idProduct, image, count);
-  count=1
-  
-}
-  
-  
-  btnDecrement.forEach((elem) =>
-    elem.addEventListener("click", handleDecrement)
-  );
+  count = 1;
+};
+
+btnDecrement.forEach((elem) => elem.addEventListener("click", handleDecrement));
 btnIncrement.forEach((elem) => elem.addEventListener("click", handleIncrement));
-productAddBtn.forEach((elem) => elem.addEventListener("click", addProductToCart));
+productAddBtn.forEach((elem) =>
+  elem.addEventListener("click", addProductToCart)
+);
 // productRemoveBtn.forEach((elem) =>
 //   elem.addEventListener("click", productRemove)
 // );
