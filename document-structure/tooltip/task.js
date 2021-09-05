b7a712d8af0238f8e1c98e3965aaa7aab0a23587
Alexplus77 <alexplus77@mail.ru>
2021-09-05 21:26:18 +0200

    gggg

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,26 +1,33 @@
 const tooltipLinkAll = [...document.querySelectorAll(".has-tooltip")];
-const body=document.querySelector('body')
-const markupTooltip = (text, left, top) => {
-    const divTooltip = document.createElement('div')
-    divTooltip.classList.add("tooltip");
-    divTooltip.classList.add('tooltip_active')
-    divTooltip.setAttribute('style', `left:${left}; top:${top}`)
-    divTooltip.innerHTML=text
-   body.appendChild(divTooltip)
-}
+const body = document.querySelector("body");
+const markupTooltip = (text) => {
+  const divTooltip = document.createElement("div");
+  divTooltip.classList.add("tooltip");
+  divTooltip.innerHTML = text;
+  body.appendChild(divTooltip);
+};
 const handleTooltip = (e) => {
-    e.preventDefault()
-    console.log(e.target)
-    const textTooltip = e.target.getAttribute("title");   
-    const findLink=tooltipLinkAll.find(elem=>elem.getAttribute('title')===textTooltip)
-    if (findLink) {
-        markupTooltip(textTooltip, leftPosition, topPosition)
-        const tooltipDiv = document.querySelector('.tooltip')
-        const leftPosition = e.target.getBoundingClientRect().x
-        const topPosition = e.target.getBoundingClientRect().y
-        console.log(e.target.getBoundingClientRect())
-}
+  e.preventDefault();
+  console.log(e.target);
+  const textTooltip = e.target.getAttribute("title");
+  const findLink = tooltipLinkAll.find(
+    (elem) => elem.getAttribute("title") === textTooltip
+  );
+  if (findLink) {
+    markupTooltip(textTooltip);
+    const tooltipDiv = document.querySelector(".tooltip");
+    tooltipDiv.classList.add("tooltip_active");
+    positionTooltip(e, tooltipDiv);
+    e.target.addEventListener("blur", () => {
+      tooltipDiv.classList.remove("tooltip_active");
+    });
+  }
+};
+const positionTooltip = (e, tooltipDiv) => {
+  const leftPosition = e.target.getBoundingClientRect().x;
+  const topPosition = e.target.getBoundingClientRect().y + 30;
+  tooltipDiv.style.top = `${topPosition}px`;
+  tooltipDiv.style.left = `${leftPosition}px`;
+};
 
-   
-}
-body.addEventListener('click', handleTooltip)
+body.addEventListener("click", handleTooltip);
