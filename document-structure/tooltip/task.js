7e6388460401f8b641eae249a1b808734b455e79
Alexplus77 <alexplus77@mail.ru>
2021-09-05 22:30:07 +0200

    kuhg

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,39 +1,43 @@
 const tooltipLinkAll = [...document.querySelectorAll(".has-tooltip")];
 const body = document.querySelector("body");
-const markupTooltip = (text) => {
+
+const offsetTopPosition = 30;
+
+const markupTooltip = () => {
   const divTooltip = document.createElement("div");
   divTooltip.classList.add("tooltip");
-   body.appendChild(divTooltip);
+  body.appendChild(divTooltip);
 };
 
 const handleTooltip = (e) => {
   e.preventDefault();
   console.log(e.target);
   const textTooltip = e.target.getAttribute("title");
-  const findLink = tooltipLinkAll.find(
-    (elem) => elem === e.target
-  );
-    if (findLink) {     
-    markupTooltip(textTooltip);
-    const tooltipDiv = document.querySelector(".tooltip");
-      tooltipDiv.classList.toggle("tooltip_active");
-      tooltipDiv.innerHTML=textTooltip
-        positionTooltip(e, tooltipDiv);  
-    }    
+  markupTooltip(textTooltip);
+  const tooltipDiv = document.querySelector(".tooltip");
+  const findLink = tooltipLinkAll.find((elem) => elem === e.target);
+  if (findLink) {
+    tooltipDiv.classList.toggle("tooltip_active");
+    tooltipDiv.innerHTML = textTooltip;
+    positionTooltip(e, tooltipDiv);
+  } else {
+    tooltipDiv.classList.remove("tooltip_active");
+  }
 };
 
-const removeTooltip = (e) => {
-    console.log('hello')
-    const tooltipDiv = document.querySelector(".tooltip");    
-      tooltipDiv.classList.remove("tooltip_active");
-  
-}
+const removeTooltipScroll = () => {
+  if (body.contains(document.querySelector(".tooltip"))) {
+    const tooltipDiv = document.querySelector(".tooltip");
+    tooltipDiv.classList.remove("tooltip_active");
+  }
+};
+
 const positionTooltip = (e, tooltipDiv) => {
   const leftPosition = e.target.getBoundingClientRect().x;
-  const topPosition = e.target.getBoundingClientRect().y + 30;
+  const topPosition = e.target.getBoundingClientRect().y + offsetTopPosition;
   tooltipDiv.style.top = `${topPosition}px`;
   tooltipDiv.style.left = `${leftPosition}px`;
 };
 
 body.addEventListener("click", handleTooltip);
-body.addEventListener("scroll", removeTooltip);
+document.addEventListener("scroll", removeTooltip);
