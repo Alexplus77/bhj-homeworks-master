b79d00dcfb091df2cdd4dc062322e3b981c27b61
Alexplus77 <alexplus77@mail.ru>
2021-09-05 20:48:52 +0200

    fsds

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,21 +1,26 @@
 const tooltipLinkAll = [...document.querySelectorAll(".has-tooltip")];
 const body=document.querySelector('body')
-const markupTooltip = (text, selector) => {
+const markupTooltip = (text, left, top) => {
     const divTooltip = document.createElement('div')
     divTooltip.classList.add("tooltip");
     divTooltip.classList.add('tooltip_active')
+    divTooltip.setAttribute('style', `left:${left}; top:${top}`)
     divTooltip.innerHTML=text
-    selector.nextElementSibling.append(divTooltip)
+   body.appendChild(divTooltip)
 }
 const handleTooltip = (e) => {
     e.preventDefault()
+    console.log(e.target)
     const textTooltip = e.target.getAttribute("title");   
     const findLink=tooltipLinkAll.find(elem=>elem.getAttribute('title')===textTooltip)
     if (findLink) {
-        markupTooltip(textTooltip, e.target)
-        console.log(e.target.nextElementSibling)
+        markupTooltip(textTooltip, leftPosition, topPosition)
+        const tooltipDiv = document.querySelector('.tooltip')
+        const leftPosition = e.target.getBoundingClientRect().x
+        const topPosition = e.target.getBoundingClientRect().y
+        console.log(e.target.getBoundingClientRect())
 }
 
    
 }
-tooltipLinkAll.forEach(link=>link. addEventListener('click', handleTooltip))
+body.addEventListener('click', handleTooltip)
