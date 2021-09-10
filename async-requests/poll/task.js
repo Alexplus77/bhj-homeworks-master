e2f6ecfda898f4f45f491327eb105ee8499659ae
Alexplus77 <alexplus77@mail.ru>
2021-09-10 12:08:28 +0200

    lllll

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -15,41 +15,27 @@ async function getOject() {
   }
 }
 
-async function sendJsonPost() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify(jsonPost),
-    });
-    const objPost = await response.json();
-      console.log(objPost);
-      handlerPollResalt(objPost)
-  } catch (error) {
-    console.log(error);
-  }
-}
+
 
 const markupPoll = ({ data: { title, answers } }) => {
-    titlePoll.innerText = title;
-    titlePoll.classList.add("poll__answers_active");
+  titlePoll.innerText = title;
+  titlePoll.classList.add("poll__answers_active");
   answers.map((answer) => {
     const answersBtn = document.createElement("button");
-      answersBtn.classList.add("poll__answer");
-      answersBtn.setAttribute("id", Math.random());
-      answersBtn.innerText = answer;
-      answersPoll.appendChild(answersBtn);    
+    answersBtn.classList.add("poll__answer");
+    answersBtn.setAttribute("id", Math.random());
+    answersBtn.innerText = answer;
+    answersPoll.appendChild(answersBtn);
   });
 };
 
 const handlerPollAlert = (e) => {
   if (e.target.classList.contains("poll__answer")) {
     alert("Спасибо, ваш голос засчитан!");
-    createJsonPost(e);
-    sendJsonPost();    
-    }
+      createJsonPost(e);
+      console.log(jsonPost)
+    //sendJsonPost();
+  }
 };
 
 const validateAnswer = (targetId) => {
@@ -70,18 +56,31 @@ const createJsonPost = (e) => {
   }
 };
 
-const handlerPollResalt = ({ stat }) => {
-    console.log(stat)
-   const answersBtn = document.createElement("button");    
-    stat.map(elem => {
-         const divResult = document.createElement('div')
-        divResult.setAttribute('id', elem.id)
-        titlePoll.appendChild(divResult)        
-    })
-    
-    
-
-}
-
 document.addEventListener("DOMContentLoaded", getOject);
 answersPoll.addEventListener("click", handlerPollAlert);
+
+// async function sendJsonPost() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//       method: "POST",
+//       headers: {
+//         "content-type": "application/json; charset=UTF-8",
+//       },
+//       body: JSON.stringify(jsonPost),
+//     });
+//     const objPost = await response.json();
+//       console.log(objPost);
+//       handlerPollResalt(objPost)
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// const handlerPollResalt = ({ stat }) => {
+//     console.log(stat)
+//    const answersBtn = document.createElement("button");
+//     stat.map(elem => {
+//          const divResult = document.createElement('div')
+//         divResult.setAttribute('id', elem.id)
+//         titlePoll.appendChild(divResult)
+//     })}
