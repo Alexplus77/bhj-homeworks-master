992755ef2937c50fb7ef17e54fb2d7516e445756
Alexplus77 <alexplus77@mail.ru>
2021-09-10 11:46:23 +0200

    dgbfbf

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,18 +1,87 @@
-// const responseObj= fetch("https://netology-slow-rest.herokuapp.com/poll.php").then(
-//     response => {
-//         if (response.status !== 200) {
-//             console.log('У нас проблемы... код' + response.status)
-//             return
-//         }
-//         response.json().then(myObj=>console.log( myObj)) }
-    
-// ).catch(err => console.log('Ошибка', err))
+const titlePoll = document.querySelector(".poll__title");
+const answersPoll = document.querySelector(".poll__answers");
 
-async function f1() {
-    let  response  = await fetch("https://netology-slow-rest.herokuapp.com/poll.php");
-    let obj = await response.json();
-   const {data:{answers:arr, title:title}={}}=obj || {}
-console.log(arr, title)
+let jsonPost = { stat: [] };
+
+async function getOject() {
+  try {
+    const response = await fetch(
+      "https://netology-slow-rest.herokuapp.com/poll.php"
+    );
+    const obj = await response.json();
+    markupPoll(obj);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
- f1()
+async function sendJsonPost() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(jsonPost),
+    });
+    const objPost = await response.json();
+      console.log(objPost);
+      handlerPollResalt(objPost)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const markupPoll = ({ data: { title, answers } }) => {
+    titlePoll.innerText = title;
+    titlePoll.classList.add("poll__answers_active");
+  answers.map((answer) => {
+    const answersBtn = document.createElement("button");
+      answersBtn.classList.add("poll__answer");
+      answersBtn.setAttribute("id", Math.random());
+      answersBtn.innerText = answer;
+      answersPoll.appendChild(answersBtn);    
+  });
+};
+
+const handlerPollAlert = (e) => {
+  if (e.target.classList.contains("poll__answer")) {
+    alert("Спасибо, ваш голос засчитан!");
+    createJsonPost(e);
+    sendJsonPost();    
+    }
+};
+
+const validateAnswer = (targetId) => {
+  return jsonPost.stat.every((elem) => elem.id !== targetId);
+};
+
+const createJsonPost = (e) => {
+  const targrtId = e.target.getAttribute("id");
+  if (jsonPost.stat.length === 0 || validateAnswer(targrtId)) {
+    jsonPost.stat.push({
+      id: targrtId,
+      answer: e.target.innerText,
+      votes: 1,
+    });
+  } else {
+    const findAnswer = jsonPost.stat.find((elem) => elem.id === targrtId);
+    findAnswer.votes++;
+  }
+};
+
+const handlerPollResalt = ({ stat }) => {
+    console.log(stat)
+   const answersBtn = document.createElement("button");    
+    stat.map(elem => {
+         const divResult = document.createElement('div')
+        divResult.setAttribute('id', elem.id)
+        titlePoll.appendChild(divResult)        
+    })
+    
+    
+
+}
+
+document.addEventListener("DOMContentLoaded", getOject);
+answersPoll.addEventListener("click", handlerPollAlert);
