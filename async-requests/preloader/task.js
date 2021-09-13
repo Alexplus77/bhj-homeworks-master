5fa203d02d186d2375d2054e372bda07c343a6be
Alexplus77 <alexplus77@mail.ru>
2021-09-13 21:14:55 +0200

    jhghjhkh

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -5,7 +5,7 @@ const markupCurrencyItem = (code, value) => {
   const itemDiv = document.createElement("div");
   itemDiv.classList.add("item");
   currenciesDiv.appendChild(itemDiv);
-  
+
   const itemCode = document.createElement("div");
   itemCode.classList.add("item__code");
   itemCode.innerText = code;
@@ -17,7 +17,7 @@ const markupCurrencyItem = (code, value) => {
   itemDiv.appendChild(itemValue);
 };
 
-(async () => {
+const getDataResponse = async () => {
   try {
     const responseData = await fetch(
       "https://netology-slow-rest.herokuapp.com"
@@ -27,9 +27,22 @@ const markupCurrencyItem = (code, value) => {
   } catch (error) {
     console.warn(error);
   }
+};
+
+(() => {
+  const valute = JSON.parse(localStorage.getItem("valute"));
+  Object.values(valute).map(({ CharCode, Value }) =>
+    markupCurrencyItem(CharCode, Value)
+  );
+  getDataResponse();
 })();
 
 const processingRespons = ({ response: { Valute: data } }) => {
   loader.classList.remove("loader_active");
-  return  Object.values(data).map(({ CharCode, Value }) => markupCurrencyItem(CharCode, Value));
+  const currencyDiv = [...document.querySelectorAll(".item")];
+  currencyDiv.forEach((elem) => elem.remove());
+  localStorage.setItem("valute", JSON.stringify(data));
+  return Object.values(data).map(({ CharCode, Value }) =>
+    markupCurrencyItem(CharCode, Value)
+  );
 };
