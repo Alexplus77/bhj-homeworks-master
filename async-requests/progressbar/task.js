97aa02870809736a19243a05fc3faf231b5091fe
Alexplus77 <alexplus77@mail.ru>
2021-09-15 16:49:21 +0200

    svdbf

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,17 +1,25 @@
-const dataFile = document.querySelector('input[type=file]')
-const getRespons = async () => {
-    try {
-       const dataPost = new FormData
-        dataPost.append('file', dataFile)
-        const respons = await fetch("https://netology-slow-rest.herokuapp.com/upload.php", {
+
+const send = document.querySelector('#send')
+const progress = document.querySelector('#progress')
+console.log()
+   
+
+const getRespons = async (e) => {   
+    try {        
+        const response = await fetch("https://netology-slow-rest.herokuapp.com/upload.php", {
             method: 'POST',
-           body:dataPost
-        });       
-const dataRes= await respons.json()
-        console.log(dataRes)
+           body: new FormData()
+        });
+        
+const dataRes =  response.body.getReader();
+        const { done, value } = await dataRes.read()
+        console.log(value.length)
+        
     } catch (error) {
         console.log(error)
     }
 }
-
-getRespons()
+send.addEventListener('click', (e) => {
+e.preventDefault()
+    getRespons()
+ })
