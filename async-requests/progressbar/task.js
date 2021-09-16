4e500ffaf0568e3737e40646ccc5f8181d848f29
Alexplus77 <alexplus77@mail.ru>
2021-09-16 08:39:22 +0200

    dcxvcbfg

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,25 +1,19 @@
+const progress = document.querySelector("#progress");
+const form = document.querySelector("#form");
 
-const send = document.querySelector('#send')
-const progress = document.querySelector('#progress')
-console.log()
-   
 
-const getRespons = async (e) => {   
-    try {        
-        const response = await fetch("https://netology-slow-rest.herokuapp.com/upload.php", {
-            method: 'POST',
-           body: new FormData()
-        });
-        
-const dataRes =  response.body.getReader();
-        const { done, value } = await dataRes.read()
-        console.log(value.length)
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
-send.addEventListener('click', (e) => {
-e.preventDefault()
-    getRespons()
- })
+
+const url = "https://netology-slow-rest.herokuapp.com/upload.php";
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.addEventListener("progress", (e) => {
+    progress.value = e.loaded / 1000000;
+  });
+
+  const formData = new FormData(form);
+  xhr.send(formData);
+};
+
