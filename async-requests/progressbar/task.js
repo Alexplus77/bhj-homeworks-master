2a68c30f3634822e0a5f5a5d633901a9d2b13681
Alexplus77 <alexplus77@mail.ru>
2021-09-17 07:25:50 +0200

    вепапра

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,17 +1,1 @@
-const dataFile = document.querySelector('input[type=file]')
-const getRespons = async () => {
-    try {
-       const dataPost = new FormData
-        dataPost.append('file', dataFile)
-        const respons = await fetch("https://netology-slow-rest.herokuapp.com/upload.php", {
-            method: 'POST',
-           body:dataPost
-        });       
-const dataRes= await respons.json()
-        console.log(dataRes)
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-getRespons()
