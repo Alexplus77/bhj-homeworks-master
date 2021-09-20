3b01cf7364f238d4db79126e91d613002d603d1b
Alexplus77 <alexplus77@mail.ru>
2021-09-20 20:10:10 +0200

    ghyjhgikhuio

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,6 +2,7 @@ const form = document.querySelector("#signin__form");
 const sendBtn = document.querySelector(".btn");
 const greeting = document.querySelector(".welcome");
 const checkbox = document.querySelector(".checkbox");
+const logoutBtn = document.querySelector(".logout");
 
 const url = "https://netology-slow-rest.herokuapp.com/auth.php";
 
@@ -20,29 +21,38 @@ const sendForm = async (e) => {
 };
 
 const onsetGriting = (text) => {
+  form.closest(".signin").classList.remove("signin_active");
+  logoutBtn.classList.remove("logoutHide");
   greeting.classList.add("welcome_active");
   greeting.children[0].innerText = text;
 };
 
 const validateUser = ({ success, user_id }) => {
   if (success) {
-    checkbox.children[0].checked
-      ? localStorage.setItem("userId", JSON.stringify(user_id))
-      : localStorage.clear();
-    form.closest(".signin").classList.remove("signin_active");
+    if (checkbox.children[0].checked) {
+      localStorage.setItem("userId", JSON.stringify(user_id));
+      }
+      
     onsetGriting(user_id);
-    form.reset();
   } else {
     alert("Неверный логин/пароль");
+    form.reset();
   }
+};
+
+const logoutUser = () => {
+  localStorage.clear();
+  logoutBtn.classList.add("logoutHide");
+  greeting.classList.remove("welcome_active");
+  form.closest(".signin").classList.add("signin_active");
 };
 
 (() => {
   const userId = JSON.parse(localStorage.getItem("userId"));
-  form.closest(".signin").classList.add("signin_active");
   if (userId) {
     onsetGriting(userId);
   }
 })();
 
 sendBtn.addEventListener("click", sendForm);
+logoutBtn.addEventListener("click", logoutUser);
