de95e8a58800177f466f81d84452386e2a80cda3
Alexplus77 <alexplus77@mail.ru>
2021-09-21 13:54:52 +0200

    ?,MM;l;l

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,29 +1,28 @@
-const card = document.querySelector('.card')
-const removeBtn = document.querySelector('.remove')
+const card = document.querySelector(".card");
+const removeBtn = document.querySelector(".remove");
 
 const saveText = () => {
-  
-    const text = card.children[0].value
-    localStorage.setItem('value', JSON.stringify(text))
-    removeBtn.classList.remove('removeHide')
-    if (!text.trim()) {
-      removeBtn.classList.add("removeHide");
-    }  
-}
- 
-const removeText = () => {
-    localStorage.clear()
-    card.children[0].value = ''
+  const text = card.children[0].value;
+  localStorage.setItem("value", JSON.stringify(text));
+  removeBtn.classList.remove("removeHide");
+  if (!text.trim()) {
     removeBtn.classList.add("removeHide");
-}
+    removeBtn.setAttribute("disabled");
+  }
+};
 
+const removeText = () => {
+  localStorage.clear();
+  card.children[0].value = "";
+  removeBtn.classList.add("removeHide");
+};
 
 (() => {
-    card.children[0].value = JSON.parse(localStorage.getItem('value'))
-    if (card.children[0].value.trim()) {
-      removeBtn.classList.remove("removeHide");
-  }  
-})()
+  card.children[0].value = JSON.parse(localStorage.getItem("value"));
+  if (card.children[0].value.trim()) {
+    removeBtn.classList.remove("removeHide");
+  }
+})();
 
-card.addEventListener('input', saveText)
-removeBtn.addEventListener('click', removeText)
+card.addEventListener("input", saveText);
+removeBtn.addEventListener("click", removeText);
