94a4b485a1b5f2ab61aa68b595350a2388b43c79
Alexplus77 <alexplus77@mail.ru>
2021-09-21 13:31:21 +0200

    fgfghgjfg

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,15 +2,23 @@ const card = document.querySelector('.card')
 const removeBtn = document.querySelector('.remove')
 
 const saveText = () => {
+  
     const text = card.children[0].value
     localStorage.setItem('value', JSON.stringify(text))
     removeBtn.classList.remove('removeHide')
+    if (!card.children[0].value.trim()) {
+      removeBtn.classList.add("removeHide");
+    }  
 }
  
 const removeText = () => {
     localStorage.clear()
     card.children[0].value = ''
     removeBtn.classList.add("removeHide");
+}
+
+const valideteTextArea = () => {
+    
 }
 
 (() => {
